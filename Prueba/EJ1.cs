83602c61e375f5ebe819d036cd9020dfab79694f
Consorcio <Consorcio@CFRSET067>
2022-06-13 13:08:42 +0200

    Agregar archivos de proyecto.

---
+++ Prueba/EJ1.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApplicacion6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!!!");
+            Console.ReadKey(); // Para la ejecucion hasta presionar tecla
+        }
+    }
+
+}
+
+
+
+
+
